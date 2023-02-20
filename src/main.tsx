121023c74dabae67126262e6373f3e0a4daf1c70
Mauran <thomas.mauran@etu.umontpellier.fr>
2023-02-20 18:57:50 +0100

    Fix(router): removing template style and adding outlet

--- src/main.tsx
+++ src/main.tsx
@@ -7,19 +7,12 @@ import "./index.css";
 import Router from "./router/router";
 import Navbar from "./components/navbar/Navbar";
 
-import themes from './themes/index.js';
 
-import { CssBaseline, StyledEngineProvider } from "@mui/material";
-import { ThemeProvider } from "@mui/material/styles";
 
 
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={themes}>
         <RouterProvider router={Router} />
-      </ThemeProvider>
-    </StyledEngineProvider>
   </React.StrictMode>
 );
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,17 +1,24 @@
 import React from "react";
-import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Outlet, Route } from "react-router-dom";
 
 import Navbar from "../components/navbar/Navbar";
-import NotFoundView from "../views/NotFound/NotFoundView"
+import NotFoundView from "../views/NotFound/NotFoundView";
 
 import WorkspaceView from "../views/Workspace/WorkspaceView";
+import LoginView from "../views/Login/LoginView";
 
-const AppLayout = () => <Navbar />;
+const AppLayout = () => (
+  <>
+    <Navbar />
+    <Outlet/>
+  </>
+);
 
 const Router = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={<AppLayout />} errorElement={<NotFoundView/>}>
+    <Route element={<AppLayout />} errorElement={<NotFoundView />}>
       <Route path="/" element={<WorkspaceView />} />
+      <Route path="/login" element={<LoginView />} />
       <Route path="/workspace" element={<WorkspaceView />} />
     </Route>
   )
