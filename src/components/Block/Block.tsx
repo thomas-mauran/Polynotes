44d9a9fc215b79c1352161d9f6ce607836809ecc
Mauran <thomas.mauran@etu.umontpellier.fr>
2023-02-23 23:49:51 +0100

    Refactor(block / pages): page is doing the logic for block

--- src/components/Block/Block.tsx
+++ src/components/Block/Block.tsx
@@ -3,82 +3,52 @@ import { useEffect, useRef, useState } from "react";
 import ContentEditable from "react-contenteditable";
 import React from "react";
 
-export default function Block() {
+export default function Block(props) {
   const [state, setState] = useState({
-    html: "Hello <i>World</i>",
-    helperOpen: false,
-    helperPosition: {
-      top: 0,
-      left: 2200,
-    },
+    html: props.defaultValue,
   });
   const ref = React.createRef() as any;
 
   useEffect(() => {
-    const selection = window.getSelection();
-    if (selection?.rangeCount !== 0) {
-      const range = selection?.getRangeAt(0).cloneRange();
-      range?.collapse(true);
-      const rect = range?.getClientRects()[0];
-      if (rect) {
-        const left = rect.left ? rect.left : 0;
-        const top = rect.top ? rect.top : 0;
-        const newPos = {
-          top: top,
-          left: left,
-        };
-          setState((prevState) => ({
-            ...prevState,
-            helperPosition: newPos,}))
-          
-      }
-    }
-    console.log(state.helperPosition);
-  }, [state.helperOpen]);
+    ref.current.focus();
+  }, []);
 
-  const handleKeyUp = (e: any) => {
+
+  const handleKeyDown = (e: any) => {
     if (e.key === "/") {
-      setState((prevState) => ({
-        ...prevState,
-        helperOpen: true,
-      }));
-    }else if (e.key === " "){
-      console.log('spacee')
-    } 
-    else {
-      e.preventDefault()
-      setState((prevState) => ({
-        ...prevState,
-        helperOpen: false,
-      }));
+      props.onSlash(true);
     }
-  };
+    if (e.key === "Enter") {
+      e.preventDefault();
+      props.onEnter(true);
+    }
+    if (e.key === "ArrowUp") {
+      e.preventDefault();
+      props.onArrowUp(props.index);
+    }  };
 
   const handleChange = (e: { target: { value: any } }) => {
     setState((prevState) => ({
       ...prevState,
       html: e.target.value,
     }));
+    props.onChange(props.index, state.html)
+
   };
 
-
-
-  const handleClose = () => {
+  const appendHtml = (blockType: string) => {
+    let html = state.html;
+    html += "<h1></h1>";
     setState((prevState) => ({
       ...prevState,
-      helperOpen: false,
+      html: html,
     }));
+    console.log(html);
   };
 
   return (
     <Box>
-      <Menu anchorOrigin={{ vertical: "bottom", horizontal: "center" }} transformOrigin={{ vertical: "top", horizontal: "center" }} open={state.helperOpen} anchorPosition={state.helperPosition} onClose={handleClose}>
-          <MenuItem><Typography variant="h2">H1</Typography></MenuItem>
-          <MenuItem><Typography variant="h3">H2</Typography></MenuItem>
-          <MenuItem><Typography variant="h4">h3</Typography></MenuItem>
-          <MenuItem><Typography variant="h6">normal text</Typography></MenuItem>
-      </Menu>
-      <ContentEditable innerRef={ref} html={state.html} onChange={handleChange} onKeyUp={handleKeyUp} style={{ fontSize: "1.3em" }} />
+      <ContentEditable innerRef={ref} html={state.html} onChange={handleChange} onKeyDown={handleKeyDown} style={{ fontSize: "1.3em" }} />
     </Box>
   );
 }
--- src/views/Page/PageView.tsx
+++ src/views/Page/PageView.tsx
@@ -1,5 +1,8 @@
 import styled from "@emotion/styled";
-import { Box, Container } from "@mui/material";
+import { Box, Container, Menu, MenuItem } from "@mui/material";
+import React, { useRef } from "react";
+import { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
 import Block from "../../components/Block/Block";
 export default function PageView() {
   const Box = styled.html`
@@ -8,10 +11,110 @@ export default function PageView() {
     margin: 5% 20%;
   `;
 
+  // const blocks: any[] = ['test']
+
+  const [blocks, setBlocks] = useState(["first", "second"]);
+
+  const [helperState, setHelperState] = useState({
+    helperOpen: false,
+    helperPosition: {
+      top: 0,
+      left: 0,
+    },
+  });
+
+  const lastBlockRef = useRef();
+
+  // useEffect(() => {
+  //   const selection = window.getSelection();
+  //   if (selection?.rangeCount !== 0) {
+  //     const range = selection?.getRangeAt(0).cloneRange();
+  //     range?.collapse(true);
+  //     const rect = range?.getClientRects()[0];
+  //     if (rect) {
+  //       const left = rect.left ? rect.left : 0;
+  //       const top = rect.top ? rect.top : 0;
+  //       const newPos = {
+  //         top: top,
+  //         left: left,
+  //       };
+  //       setHelperState((prevState) => ({
+  //         ...prevState,
+  //         helperPosition: newPos,
+  //       }));
+  //     }
+  //   }
+  // }, [helperState.helperOpen]);
+
+
+  // Functions
+  const handleClickMenu = (e) => {
+    setHelperState((prevState) => ({
+      ...prevState,
+      helperOpen: false,
+    }));
+  };
+
+  const handleOpen = () => {
+    setHelperState((prevState) => ({
+      ...prevState,
+      helperOpen: true,
+    }));
+  };
+
+  const handleClose = () => {
+    setHelperState((prevState) => ({
+      ...prevState,
+      helperOpen: false,
+    }));
+  };
+
+  const handleCreateNewBlock = () => {
+    const newList = [...blocks, ""];
+    setBlocks(newList);
+  };
+
+  const handleUpdateHtml = (index, html) => {
+    console.log(index)
+    const newBlockList = blocks
+    newBlockList[index] = html
+    setBlocks(newBlockList);
+  };
+
+  const handleArrowUp = () =>{
+    //todo
+  }
+
+  const blockElements = blocks.map((item, index) => {
+    return <Block key={index} 
+    defaultValue={item} 
+    onEnter={handleCreateNewBlock} 
+    onSlash={handleOpen} 
+    index={index} 
+    onChange={handleUpdateHtml}
+    onArrowUp={handleArrowUp}
+    ></Block>;
+  });
+
   return (
     <Container fixed>
-      <Box>
-        <Block />
+      <Box id="mainBlock">
+        <Menu anchorOrigin={{ vertical: "bottom", horizontal: "center" }} transformOrigin={{ vertical: "top", horizontal: "center" }} open={helperState.helperOpen} anchorPosition={helperState.helperPosition} onClose={handleClose}>
+          <MenuItem id="h2" onClick={handleClickMenu}>
+            H1
+          </MenuItem>
+          <MenuItem id="h3" onClick={handleClickMenu}>
+            H2
+          </MenuItem>
+          <MenuItem id="h4" onClick={handleClickMenu}>
+            h3
+          </MenuItem>
+          <MenuItem id="h6" onClick={handleClickMenu}>
+            normal text
+          </MenuItem>
+        </Menu>
+        {/* <Block onEnter={handleCreateNewBlock}/> */}
+        {blockElements}
       </Box>
     </Container>
   );
