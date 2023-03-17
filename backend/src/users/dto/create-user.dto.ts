1559e63e403a78598c4eb38e7dea6c31b76191da
Mauran <thomas.mauran@etu.umontpellier.fr>
2023-03-17 08:21:06 +0100

    Fix(CreateUser): Fix Dto

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,15 +1,24 @@
-import { IsEmail, IsNotEmpty, MaxLength } from 'class-validator'
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsStrongPassword,
+  isStrongPassword,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
-export class CreateUserDto{
-    @IsNotEmpty()
-    @MaxLength(50)
-    readonly username: string;
+export class CreateUserDto {
+  @IsNotEmpty()
+  @MaxLength(50)
+  @MinLength(4)
+  readonly username: string;
 
-    @IsNotEmpty()
-    @IsEmail()
-    readonly email: string; 
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email: string;
 
-    @IsNotEmpty()
-    @MaxLength(255)
-    readonly password: string;
+  @IsNotEmpty()
+  @MaxLength(255)
+  @MinLength(4)
+  readonly password: string;
 }
