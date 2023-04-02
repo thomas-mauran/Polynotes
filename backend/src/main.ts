48ea3a65993f5a640a32f976dba3d617243aed2d
Mauran <thomas.mauran@etu.umontpellier.fr>
2023-04-02 13:57:01 +0200

    Fix(Backend): removing /v1

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1/');
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({ origin: process.env.FRONTEND_ORIGIN, credentials: true });
   await app.listen(3000);
