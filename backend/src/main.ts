b0f59537b0789e712a7c1162b414e696d6bbb34d
Mauran <thomas.mauran@etu.umontpellier.fr>
2023-04-02 12:45:05 +0200

    Fix(Argocd): backend endpoint

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('v1/');
+  app.setGlobalPrefix('api/v1/');
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({ origin: process.env.FRONTEND_ORIGIN, credentials: true });
   await app.listen(3000);
